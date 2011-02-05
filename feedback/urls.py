e989356f5bedb5b601b4d38dd7ff92774162edd2
Alex Kamedov <alex@kamedov.ru>
2011-02-05 22:28:38 +0500

    add option for redirect to simple page after successful form sending

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
 
 from feedback.views import leave_feedback
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^$', leave_feedback, name='leave-feedback'),
+    url(r'^sended/$', direct_to_template, {'template': 'feedback/sended.html'}, name='feedback_sended'),
 )
--- feedback/views.py
+++ feedback/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
-from django.contrib import messages
+from django.conf import settings
 
 from feedback.forms import FeedbackForm
+from django.core.urlresolvers import reverse
 
 def leave_feedback(request):
     form = FeedbackForm(request.POST or None)
@@ -12,7 +13,12 @@ def leave_feedback(request):
         feedback = form.save(commit=False)
         if request.user.is_authenticated():
             feedback.user = request.user
-            messages.add_message(request, messages.INFO, _("Your feedback has been saved successfully."))
+            if not getattr(settings, 'FEEDBACK_ON_SINGLE_PAGE', False):
+                from django.contrib import messages
+                messages.add_message(request, messages.INFO, _("Your feedback has been saved successfully."))
+                next = request.GET.get('next', request.META.get('HTTP_REFERER', '/'))
+            else:
+                next = reverse('feedback_sended')
         feedback.save()
-        return HttpResponseRedirect(request.GET.get('next', request.META.get('HTTP_REFERER', '/')))
+        return HttpResponseRedirect(next)
     return render_to_response('feedback/feedback_form.html', {'form': form}, context_instance=RequestContext(request))
