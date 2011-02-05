fa2a88108bf3b1c1dc82b4319e26489929adb070
Alex Kamedov <alex@kamedov.ru>
2011-02-05 21:57:16 +0500

    add localization support

--- feedback/models.py
+++ feedback/models.py
@@ -1,18 +1,20 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 class Feedback(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, verbose_name=_('User'))
+    type = models.CharField(_('Type'), choices=settings.FEEDBACK_CHOICES, max_length=100)
+    message = models.TextField(_('Message'))
+    time = models.DateTimeField(_('Time'), auto_now_add=True)
 
     class Meta:
-
         ordering = ['-time']
-
-    user    = models.ForeignKey(User, blank=True, null=True)
-    type    = models.CharField(choices=settings.FEEDBACK_CHOICES, max_length=100)
-    message = models.TextField()
-    time    = models.DateTimeField(auto_now_add=True)
+        verbose_name = _('Feedback')
+        verbose_name_plural = _('Feedbacks')
 
     def __unicode__(self):
         return self.message
